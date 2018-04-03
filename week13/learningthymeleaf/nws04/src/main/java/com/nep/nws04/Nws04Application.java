8e83a467a4d590c9a8c54fa9f2e0b73e11b0db74
Jailani Abdul Rahman <jailani.har@googlemail.com>
2018-04-03 12:02:12 +0800

    Week 13 Java Web Programming (Slot 1)

---
+++ week13/learningthymeleaf/nws04/src/main/java/com/nep/nws04/Nws04Application.java
@@ -0,0 +1,12 @@
+package com.nep.nws04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nws04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nws04Application.class, args);
+	}
+}
