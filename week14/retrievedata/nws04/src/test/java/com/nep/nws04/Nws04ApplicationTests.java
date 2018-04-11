743fdf99b473e56a337daf2eb3218b30fa06cd04
Jailani Abdul Rahman <jailani.har@googlemail.com>
2018-04-11 18:39:25 +0800

    Week 14 Java Web Application (Slot 2)

---
+++ week14/retrievedata/nws04/src/test/java/com/nep/nws04/Nws04ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nep.nws04;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Nws04ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
